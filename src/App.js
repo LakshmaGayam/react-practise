a9280aff7f22da84775878fdabad3af55e099910
LakshmaGayam <51111017+LakshmaGayam@users.noreply.github.com>
2022-09-07 14:27:34 +0530

    3 files modified, 2 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {  Routes, Route } from "react-router-dom";
 import DashBoard from './dashboard'
 import Product from './product';
 import Home from './home';
+import ErrorPage from './404.js'
 export default function App() {
   return (
     <div>
@@ -13,7 +14,7 @@ export default function App() {
           <Route path='/dashboard' element={<DashBoard/>}/>
           <Route path='/products' element={<Product/>}/>
           <Route path='/home' element={<Home/>}/>
-
+          <Route path="*" element={<ErrorPage/>}/>
         </Routes>
     </div>
   );
--- src/nav.js
+++ src/nav.js
@@ -15,8 +15,8 @@ export default function NavBar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link  id="RouterNavLink" ><Link to='/dashboard' className='link-c'>Dashboard</Link></Nav.Link>
-              <Nav.Link ><Link to='/home'>Home</Link></Nav.Link>
-              <Nav.Link><Link to='/products'>Products</Link></Nav.Link>
+              <Nav.Link id='2'><Link to='/home'>Home</Link></Nav.Link>
+              <Nav.Link id='3'><Link to='/products'>Products</Link></Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
